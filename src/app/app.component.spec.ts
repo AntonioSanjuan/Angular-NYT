91a18b5bee9e928ebc59226b3698d5c077b8ab43
Tony <Tony@DESKTOP-ENEUN8I>
2021-08-25 22:20:23 +0200

    refactoring

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it("should have as title 'AngularNYT'", () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularNYT');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'AngularNYT app is running!'
-    );
-  });
 });

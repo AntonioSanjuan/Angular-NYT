7d131f52f865c0a7301344a7099cb5dd289add1d
Tony Sanjuan <tonysanj13@gmail.com>
2022-01-19 15:14:59 +0100

    refactor: some names and typps

--- src/app/components/common/search/search.component.ts
+++ src/app/components/common/search/search.component.ts
@@ -15,8 +15,12 @@ export class SearchComponent {
 
   constructor() { }
 
-  search() {
+  toggleSearch() {
     this.switchSearchStatus();
+  }
+
+  search() {
+    
   }
 
   private switchSearchStatus() {
---
+++ src/app/models/internal/inoutStatus/inoutStatus.model.ts
@@ -0,0 +1,4 @@
+export enum InoutStatus {
+  Opened = 'in',
+  Closed = 'out',
+}
